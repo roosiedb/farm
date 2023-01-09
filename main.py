bf6c8d2e1feacb1a9cdbccb85078a9b377e0da0a
LiekeG9 <109586922+LiekeG9@users.noreply.github.com>
2023-01-09 20:07:26 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# farm application
+import power
+
+def main():
+    print(power.power(3,3))
+
+if __name__ == "__main__": 
+    main()
---
+++ test_main.py
@@ -0,0 +1,8 @@
+import power
+
+def test_1():
+    assert power.power(2,2) == 4
+    assert power.power(2,4) == 16
+    assert power.power(3,2) == 9
+    assert power.power(3,3) == 27
+    
